7d3178087cacdae7f23f7b58c7f5cac0fc2223e8
David do Nascimento Santos <01756847401@ifrn.local>
2019-10-22 10:56:09 -0300

    colocando index no render

--- projeto_chocolatey/helloworld.py
+++ projeto_chocolatey/helloworld.py
@@ -1,12 +1,20 @@
 from django.conf.urls import url
 from django.http import HttpResponse
+from django.shortcuts import render
 
 SECRET_KEY = 'helloworld'
 DEBUG = True
 ROOT_URLCONF = __name__
+TEMPLATES = [
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'APP_DIRS': True,
+		'DIRS': 'C:\\Users\\01756847401\\Documents\\chocolatey-ifrn-web\\projeto_chocolatey\\templates'
+	}
+]
 
 def hello_world(request):
-	return HttpResponse('Hello world!!!')
+	return render(request, 'templates/index.html')
 
 
 urlpatterns = [
